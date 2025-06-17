5b0c6c543a6de93fad13bf106dea49796a8f4f99
Striker <sebartion77@gmail.com>
2025-06-17 11:07:52 +0700

    Merge https://github.com/rathena/rathena

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -2175,6 +2175,9 @@ static bool mob_ai_sub_hard(struct mob_data *md, t_tick tick)
 	{
 		int32 stop_flag = USW_FIXPOS|USW_RELEASE_TARGET;
 
+		// Source may die due to reflect damage
+		map_freeblock_lock();
+
 		// Hiding is a special case because it prevents normal attacks but allows skill usage
 		// TODO: Some other states also have this behavior and should be investigated
 		// TODO: EFST_AUTOCOUNTER, EFST_BLADESTOP, NPC_SR_CURSEDCIRCLE
@@ -2194,6 +2197,8 @@ static bool mob_ai_sub_hard(struct mob_data *md, t_tick tick)
 		// Stop and make sure there is no chase target when attack was not skipped
 		if (stop_flag != USW_NONE)
 			unit_stop_walking(md, stop_flag);
+
+		map_freeblock_unlock();
 
 		//Target still in attack range, no need to chase the target
 		return true;
@@ -5541,10 +5546,10 @@ uint64 MobDatabase::parseBodyNode(const ryml::NodeRef& node) {
 		mob->damagetaken = damage;
 	}
 
-	if (this->nodeExists(node, "GroupID")) {
+	if (this->nodeExists(node, "GroupId")) {
 		uint16 group_id;
 
-		if (!this->asUInt16Rate(node, "GroupID", group_id))
+		if (!this->asUInt16Rate(node, "GroupId", group_id))
 			return 0;
 
 		mob->group_id = group_id;
